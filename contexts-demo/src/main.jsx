4cdca3875b8666c2e859cc2c70fed1eec32f1e10
Peter McCarron <pcmccarron@gmail.com>
2023-02-28 10:22:16 -0700

    minified?

--- contexts-demo/src/main.jsx
+++ contexts-demo/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {App} from './App'
 import './index.css'
 import {asyncWithLDProvider} from 'launchdarkly-react-client-sdk'
 import {v4 as uuid} from 'uuid'
